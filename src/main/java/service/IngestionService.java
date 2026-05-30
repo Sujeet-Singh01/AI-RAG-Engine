5cba6b3cca7454057fb4a41d271a02f5ee0761c5
Sujeet-Singh01 <sujeet2000singhrw@gmail.com>
2026-05-31 01:14:28 +0530

    Created Ingestion Service

---
+++ src/main/java/service/IngestionService.java
@@ -0,0 +1,35 @@
+package service;
+
+
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.TextReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.stereotype.Service;
+import java.io.File;
+import java.util.List;
+
+@Service
+public class IngestionService {
+    private final VectorStore vectorStore;
+    public IngestionService(VectorStore vectorStore) {
+        this.vectorStore = vectorStore;
+    }
+
+    public void ingestLocalFile(String absoluteFilePath) {
+        FileSystemResource resource = new FileSystemResource(new File(absoluteFilePath));
+        TextReader textReader = new TextReader(resource);
+
+        textReader.getCustomMetadata().put("filename", resource.getFilename());
+        List<Document> rawDocuments = textReader.get();
+
+        TokenTextSplitter textSplitter = TokenTextSplitter.builder()
+                .withChunkSize(800)
+                .build();
+
+        List<Document> splitDocuments = textSplitter.apply(rawDocuments);
+        vectorStore.add(splitDocuments);
+
+    }
+}
