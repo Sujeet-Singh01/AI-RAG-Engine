0ed1a29a1e177cdc1fe5d76d1acdfdba4738bf19
Sujeet-Singh01 <sujeet2000singhrw@gmail.com>
2026-05-30 22:48:02 +0530

    Initial commit

---
+++ src/main/java/com/codebaserag/ai_rag_engine/AiRagEngineApplication.java
@@ -0,0 +1,13 @@
+package com.codebaserag.ai_rag_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRagEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiRagEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codebaserag/ai_rag_engine/AiRagEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebaserag.ai_rag_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiRagEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
