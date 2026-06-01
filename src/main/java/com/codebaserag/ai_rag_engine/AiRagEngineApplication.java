d942f95af9eab9715221770a507c19a9177a2966
Sujeet-Singh01 <sujeet2000singhrw@gmail.com>
2026-06-02 01:42:31 +0530

    Using gemini api for embeddings and groq for llm search

--- src/main/java/com/codebaserag/ai_rag_engine/AiRagEngineApplication.java
+++ src/main/java/com/codebaserag/ai_rag_engine/AiRagEngineApplication.java
@@ -1,12 +1,16 @@
 package com.codebaserag.ai_rag_engine;
 
+
+import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AiRagEngineApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AiRagEngineApplication.class, args);
 	}
 
